975ce4bb5c0272f04a0758e36c49d1bc882023d3
Erik Thiem <erikthiem@gmail.com>
2013-02-14 09:44:31 -0500

    added basic GUI layout

--- interfaces/gui/gui.rb
+++ interfaces/gui/gui.rb
@@ -1,7 +1,37 @@
 # NOT WORKING, USE CONSOLE INTERFACE
 
-Shoes.app {
+Shoes.app do
 	title "MassText\n"
+	
+	stack do
+		flow do
+			para "Number:\t\t\t"
+			number = edit_line
+		end
 
+		flow do
+			para "Gmail username:\t\t"
+			username = edit_line
+		end
 
-}
+		flow do
+			para "Gmail password:\t\t"
+			password = edit_line
+		end
+
+		flow do
+			para "Subject:\t\t\t\t"
+			subject = edit_line
+		end
+
+		flow do
+			para "Message:\t\t\t"
+			message = edit_line
+		end
+
+		flow do
+			button "Send"
+		end
+	end
+
+end
